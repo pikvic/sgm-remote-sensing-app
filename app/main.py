a92ca3bef1bcc20a53512c184a91d2f2a6c3ab1f
pikvic <truepikvic@gmail.com>
2021-06-09 13:24:56 +1000

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI, StaticFiles
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def root():
+    return templates.TemplateResponse("index.html")
